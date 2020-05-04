221ec16a94838ae0c7a4d2d89fbe392e4af76bba
Elijah wines <elijahwines84@yahoo.com>
2020-05-04 17:46:44 +0000

    Automatically backed up by Learn

---
+++ app/models/dog.rb
@@ -0,0 +1,19 @@
+class Dog 
+
+  attr_accessor :name, :breed, :age 
+
+  @@all = []
+
+  def initializes(name=nil, breed=nil, age=nil)
+    @name = name 
+    @breed = breed 
+    @age = age 
+
+    @@all << self 
+  end
+
+  def self.all 
+    @@all
+  end
+  
+end
